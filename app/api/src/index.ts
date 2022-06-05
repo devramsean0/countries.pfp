6c459b114aa8551080e47f265425006c02dc2af1
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-06-05 16:35:34 +0100

    Start on the api I guess

---
+++ app/api/src/index.ts
@@ -0,0 +1,44 @@
+import express, { Express, Request, Response } from 'express';
+import fetch from "node-fetch";
+import sharp from "sharp"
+const app: Express = express();
+const port = 3200
+
+app.get('/', (req: Request, res: Response) => {
+    void req
+    res.send('API server for countries.pfp');
+});
+
+app.get('/generate/:country/:position', (req: Request, res: Response) => {
+  var flagimage: any
+  ( async () => {
+    flagimage = await fetch(`localhost:3201/image/${country}/${position}`);
+  })
+  const srcImage: any = req.query.image;
+  const country = req.params.country;
+  const position  = req.params.position;
+  const buffer  = Buffer.from(srcImage, "base64");
+  sharp(buffer)
+  .resize({
+    fit: sharp.fit.contain,
+    height: 100
+  })
+  .toBuffer({ resolveWithObject: true }) 
+  .then(({data}) => {
+    sharp(flagimage)
+    .resize(300,300) 
+      .composite([{ 
+        input: data
+      }])
+        .toBuffer({ resolveWithObject: true})
+        .then(({data}) => {
+          res.json({
+            buffer: data
+          })
+          console.log(data)
+        })
+  })
+})
+app.listen(port, () => {
+  console.log(`⚡️[API] [server]: Server is running at https://localhost:${port}`);
+});
---
+++ app/b2-manager/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from 'express';
+const app: Express = express();
+const port = 3201
+const b2Addr = "https://countries-pfp.s3.us-west-004.backblazeb2.com/"
+
+app.get('/', (req: Request, res: Response) => {
+  void req
+  res.send('B2 manager server for countries.pfp');
+});
+
+app.get('/image/:country/:position', (req: Request, res: Response) => {
+  const country = req.params.country;
+  const position = req.params.position;
+  res.send(`${b2Addr}/countries/${country}/${position}.png`);
+})
+app.listen(port, () => {
+  console.log(`⚡️[API] [b2-manager]: Server is running at https://localhost:${port}`);
+});
